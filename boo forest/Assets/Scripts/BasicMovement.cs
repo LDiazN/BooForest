1885ac3326f82ce525effb2ed279493ff05703bc
xdgorgola <15-11264@usb.ve>
2021-01-28 23:46:10 -0400

    Base movement

---
+++ boo forest/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BasicMovement : MonoBehaviour
+{
+    private Rigidbody2D _rb2d;
+
+    private Vector2 _vel = Vector2.zero;
+    private bool _queued = false;
+
+
+    private void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();    
+    }
+
+
+    public void Move(Vector2 vel)
+    {
+        _queued = true;
+        _vel = vel;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (_queued)
+        {
+            _rb2d.MovePosition((Vector2)transform.position + _vel * Time.fixedDeltaTime);
+            _queued = false;
+            _vel = Vector2.zero;
+        }
+    }
+}
