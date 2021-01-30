f741f62b5736c7a689f106cb9c7c61c717278794
Luis Diaz <ldiazn98@gmail.com>
2021-01-29 20:58:59 -0400

    halp

---
+++ boo forest/Assets/Scripts/Die/DeathTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DeathTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent OnPlayerDeath;
+
+    private void Die()
+    {
+        OnPlayerDeath.Invoke();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Die();
+    }
+}
