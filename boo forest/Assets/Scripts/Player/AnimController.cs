fbcf70ff16de221cf59af2ca5aa5cc42f761c6cd
xdgorgola <15-11264@usb.ve>
2021-01-31 13:08:26 -0400

    Kitty animao

---
+++ boo forest/Assets/Scripts/Player/AnimController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+    [SerializeField]
+    private SteeringMovement _behaviour;
+
+    private bool left = false;
+    private bool right = false;
+    private bool up = false;
+    private bool down = false;
+
+
+    private void Update()
+    {
+        Vector3 vel = _behaviour.velocity;
+
+        if (Mathf.Abs(vel.x) > Mathf.Abs(vel.y))
+        {
+            if (!left && vel.x < 0)
+            {
+                left = true;
+                right = up = down = false;
+                _animator.Play("Left");
+            }
+            else if (!right && vel.x > 0)
+            {
+                right = true;
+                left = up = down = false;
+                _animator.Play("Right");
+            }
+        }
+        else
+        {
+            if (!up && vel.y > 0)
+            {
+                up = true;
+                right = down = left = false;
+                _animator.Play("Back");
+            }
+            else if (!down && vel.y < 0)
+            {
+                down = true;
+                right = left = up = false;
+                _animator.Play("Front");
+            }
+        }
+    }
+}
